bc9fc3577f7489ed2fca52766accf8b5866cce91
fitzpatricksrus <junk@fitzpatircksr.us>
2014-10-01 23:29:03 -0700

    remove build artifacts

--- T3_Blink/Release/arduino/realloc.c.d
+++
@@ -1,8 +0,0 @@
-arduino/realloc.c.d arduino/realloc.c.o:  \
- /Applications/Arduino\ 1.5.5.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/avr-libc/realloc.c \
-  /Applications/Arduino\ 1.5.5.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/avr-libc/sectionname.h \
-  /Applications/Arduino\ 1.5.5.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/avr-libc/stdlib_private.h
-
-/Applications/Arduino\ 1.5.5.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/avr-libc/sectionname.h:
-
-/Applications/Arduino\ 1.5.5.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/avr-libc/stdlib_private.h:
